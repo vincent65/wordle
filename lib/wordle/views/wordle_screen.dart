5345350c4864e63a4536d752508d09a1c5a65950
Vincent Yip <realsharkguy@gmail.com>
2022-12-02 23:04:13 -0500

    function of wordle protocol: button mappings and game logic

--- lib/wordle/views/wordle_screen.dart
+++ lib/wordle/views/wordle_screen.dart
@@ -55,4 +55,99 @@ class _WordleScreenState extends State<WordleScreen> {
       ),
     );
   }
+
+  void _onKeyTapped(String val) {
+    if (_gameStatus == GameStatus.playing) {
+      setState(() => _currentWord?.addLetter(val));
+    }
+  }
+
+  void _onDeleteTapped() {
+    if (_gameStatus == GameStatus.playing) {
+      setState(() {
+        _currentWord?.removeLetter();
+      });
+    }
+  }
+
+  void _onEnterTapped() {
+    if (_gameStatus == GameStatus.playing &&
+        _currentWord != null &&
+        !_currentWord!.letters.contains(Letter.empty())) {
+      _gameStatus = GameStatus.submitting;
+
+      for (var i = 0; i < _currentWord!.letters.length; i++) {
+        final currentWordLetter = _currentWord!.letters[i];
+        final currentSolutionLetter = _solution.letters[i];
+
+        setState(() {
+          if (currentWordLetter == currentSolutionLetter) {
+            _currentWord!.letters[i] =
+                currentWordLetter.copyWith(status: LetterStatus.correct);
+          } else if (_solution.letters.contains(currentWordLetter)) {
+            _currentWord!.letters[i] =
+                currentWordLetter.copyWith(status: LetterStatus.inWord);
+          } else {
+            _currentWord!.letters[i] =
+                currentWordLetter.copyWith(status: LetterStatus.notInWord);
+          }
+        });
+      }
+
+      _checkIfWinOrLoss();
+    }
+  }
+
+  void _checkIfWinOrLoss() {
+    if (_currentWord!.wordString == _solution.wordString) {
+      _gameStatus = GameStatus.won;
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        dismissDirection: DismissDirection.none,
+        duration: const Duration(days: 1),
+        backgroundColor: cCorrect,
+        content: const Text(
+          'You Won!',
+          style: TextStyle(color: Colors.white),
+        ),
+        action: SnackBarAction(
+          onPressed: _restart,
+          textColor: Colors.white,
+          label: 'New Game',
+        ),
+      ));
+    } else if (_currentWordIndex + 1 >= _board.length) {
+      _gameStatus = GameStatus.lost;
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        dismissDirection: DismissDirection.none,
+        duration: const Duration(days: 1),
+        backgroundColor: cCorrect,
+        content: Text(
+          'You Lost! Solution: ${_solution.wordString}',
+          style: const TextStyle(color: Colors.white),
+        ),
+        action: SnackBarAction(
+          onPressed: _restart,
+          textColor: Colors.white,
+          label: 'New Game',
+        ),
+      ));
+    } else {
+      _gameStatus = GameStatus.playing;
+    }
+    _currentWordIndex += 1;
+  }
+
+  void _restart() {
+    setState(() {
+      _gameStatus = GameStatus.playing;
+      _currentWordIndex = 0;
+      _board
+        ..clear()
+        ..addAll(List.generate(6,
+            (index) => Word(letters: List.generate(5, (_) => Letter.empty()))));
+      _solution = Word.fromString(
+        fiveLetterWords[Random().nextInt(fiveLetterWords.length)].toUpperCase(),
+      );
+    });
+  }
 }
